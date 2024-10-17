7aa6632915999516dd0b6f1ab50e211091a1e477
Antoine07 <antoine.lucsko@gmail.com>
2024-10-17 10:22:00 +0200

    analyse correction

--- analyse_correction/rpa_bot.py
+++ analyse_correction/rpa_bot.py
@@ -4,14 +4,15 @@ import numpy as np
 
 data = pd.read_csv('./Data/analyse.csv')
 
-# print(data.head(10))
-# print(data.tail(10))
+print(data.head(10))
+print(data.tail(10))
+print( data.describe())
 
 # Retirez du dataset les valeurs dont le prix est nul
 # Retirez du dataset les valeurs dont le prix est nul
 
 print( (data['price'] < 0).sum())
 
-# On nettoye ces données
-mask =
+# Nettoyage Les valeurs négatives sont une erreur de saisie 
+
 
